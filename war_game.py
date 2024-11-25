0bd4b836dd85b5ab3703a7953a87368ac7d89840
tomska888 <tomasz.kosa8@gmail.com>
2024-11-25 07:51:24 +0100

    Add Deck class to create, shuffle, and deal cards

--- war_game.py
+++ war_game.py
@@ -1,3 +1,5 @@
+import random
+
 class Card:
     def __init__(self, rank, suit):
         self.rank = rank
@@ -11,3 +13,16 @@ class Card:
 
     def __repr__(self):
         return f"{self.rank}{self.suit}"
+    
+class Deck:
+    def __init__(self):
+        suits = ['♠', '♥', '♦', '♣']
+        ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10',
+                 'J', 'Q', 'K', 'A']
+        self.cards = [Card(rank, suit) for suit in suits for rank in ranks]
+
+    def shuffle(self):
+        random.shuffle(self.cards)
+
+    def deal_half(self):
+        return self.cards[:26], self.cards[26:]
